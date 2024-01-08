a3e6ed151bf14198226b719da2dd67ebd6318e70
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-01-08 17:11:16 +0500

    Hero Section and Form

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,20 @@ let navClose = document.getElementById('pc-mobile-navbar-close');
 
 mobileToggler.addEventListener('click', ()=>{
     mobileNav.classList.add('navbar-active');
+    mobileToggler.style.display = "none";
 });
 
 navClose.addEventListener('click', () => {
     mobileNav.classList.remove('navbar-active');
+    mobileToggler.style.display = "block";
 });
+
+// forms
+function showForm(formId) {
+    // Hide all form sections
+    document.getElementById('pc-oneway-form').style.display = 'none';
+    document.getElementById('pc-bythehour-form').style.display = 'none';
+
+    // Show the selected form section
+    document.getElementById(formId).style.display = 'block';
+  }
